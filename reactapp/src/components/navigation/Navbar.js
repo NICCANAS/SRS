4da1f166107bfbf50b496281188bbc7d2eada7cd
NICCANAS <nic.canas@duocuc.cl>
2023-04-18 21:06:40 -0400

    Cambios pata

--- reactapp/src/components/navigation/Navbar.js
+++ reactapp/src/components/navigation/Navbar.js
@@ -2,8 +2,16 @@ import {connect} from'react-redux'
 
 function Navbar() {
     return(
-        <nav>
-           Navbar
+        <nav className='w-full py-10 shadow-md fixed'>
+            <div className='bg-white px-4 sm:px-6'>
+            <div className='ml-4 -mt2 flex flex-wrap items-center justify-between sm:flex-nowrap'>
+                <div className='ml-4 mt-2'>
+                <h3 className='txt-lg font-medium leading-6 text-gray-900'>Secure Recruitment Service</h3>
+                
+
+                </div>
+            </div>
+            </div>
         </nav>
     )
 }
