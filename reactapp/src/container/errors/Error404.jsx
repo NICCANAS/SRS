4b8dfc50ca98209d52d0edd5084ffe0efc27de03
Jordan101 <102256248+Jordanium01@users.noreply.github.com>
2023-04-24 18:05:05 -0400

    ola niko

--- reactapp/src/container/errors/Error404.jsx
+++ reactapp/src/container/errors/Error404.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Error404() {
     return (
         <div class="grid h-screen px-4 bg-white place-content-center">
@@ -10,11 +12,13 @@ function Error404() {
 
                 <p class="mt-4 text-gray-500">Redireccion incorrecta My homie</p>
 
+                <Link to="/">
                 <h1
                     class="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
                 >
                     Pa a'trah
                 </h1>
+                </Link>
             </div>
         </div>
     )
