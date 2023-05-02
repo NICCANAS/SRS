bf3b4eee7fdb1c6e3e507f782ddc47dd5b80a4bf
Marcos <marquitosceballos15@gmail.com>
2023-05-02 12:47:38 -0400

    .
    .

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,11 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/index.css'
+import { combineReducers } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default combineReducers({
+    
+})
