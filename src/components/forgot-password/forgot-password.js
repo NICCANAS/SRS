64c970af52ea75b0547cd1708841ce8df373ed75
Felipe Fernández <fe.fernandez@duocuc.cl>
2023-05-06 12:10:04 -0400

    Pequeños ajustes a cambiar contraseña

--- src/components/forgot-password/forgot-password.js
+++ src/components/forgot-password/forgot-password.js
@@ -32,18 +32,11 @@ function forgotPass() {
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                                 </div>
 
-                                <div>
+                                <div class="pb-3">
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirmar Nueva Contraseña</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                                 </div>
-                                <div class="flex items-start">
-                                    <div class="flex items-center h-5">
-                                        <input id="newsletter" aria-describedby="newsletter" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="" />
-                                    </div>
-                                    <div class="ml-3 text-sm">
-                                        <label for="newsletter" class="font-light text-gray-500 dark:text-gray-300">Acepto los <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terminos y Condiciones.</a></label>
-                                    </div>
-                                </div>
+                                
                                 <Link to="/login"><button type="submit" class=" mt-3 w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-primary-800">Guardar</button></Link>
                             </form>
                         </div>
