1319b27445fa4375ff53eadce26f6b57c269b36d
NICCANAS <nic.canas@duocuc.cl>
2023-05-08 22:15:43 -0400

    VIVA SHILE

--- src/components/list-services/listservs.js
+++ src/components/list-services/listservs.js
@@ -2,16 +2,12 @@ import { connect } from 'react-redux'
 import CardServs from "../../components/list-services/cardservs"
 import Barbusq from '../../components/list-services/barbusq'
 import Seguiserv from './Seguiserv'
+import PerfilUsu from './perfilusu'
 import React from 'react'
 import { useState } from 'react'
 
 function ListServs() {
-    const [IsToggled, setIsToggled] = useState(false)
-    const [isShown, setIsShown] = useState(false);
-
-    const handleClick = event => {
-        setIsShown(current => !current);
-    };
+    const [active, setActive] = useState("Cardserv")
 
     return (
         <div class="flex flex-wrap bg-gray-100 w-full h-screen">
@@ -32,7 +28,7 @@ function ListServs() {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
                             </span>
-                            <span onClick={() => setIsToggled(!IsToggled)}>Inicio</span>
+                            <span onClick={() => setActive("Cardserv")}>Inicio</span>
                         </a>
                     </li>
                     <li>
@@ -42,7 +38,7 @@ function ListServs() {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                                 </svg>
                             </span>
-                            <span onClick={() => setIsToggled(!IsToggled)}>Seguimiento servicios</span>
+                            <span onClick={() => setActive("SeguirServ")}>Seguimiento servicios</span>
                         </a>
                     </li>
 
@@ -53,7 +49,7 @@ function ListServs() {
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                 </svg>
                             </span>
-                            <span onClick={handleClick} >Mi Perfil</span>
+                            <span onClick={() => setActive("Cambiarperfil")} >Mi Perfil</span>
                         </a>
                     </li>
                     <li>
@@ -82,7 +78,9 @@ function ListServs() {
             <div class="w-9/12">
                 <div class="p-4 text-gray-500">
                     <Barbusq />
-                    {IsToggled ? <Seguiserv /> : <CardServs />}
+                    {active === "Cardserv" && <CardServs/>}
+                    {active === "SeguirServ" && <Seguiserv/>}
+                    {active === "Cambiarperfil" && <PerfilUsu/>}
                     
                 </div>
             </div>
--- src/components/list-services/perfilusu.js
+++ src/components/list-services/perfilusu.js
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux'
 
-function SeguirServ() {
+function PerfilUsu() {
     return (
-        <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-            <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Account settings</h2>
+        <section class="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md mt-6 dark:bg-gray-800">
+            <h2 class="text-lg font-semibold text-gray-700 capitalize dark:text-white">Configuración perfil</h2>
 
             <form>
                 <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
@@ -13,23 +13,28 @@ function SeguirServ() {
                     </div>
 
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Email Address</label>
+                        <label class="text-gray-700 dark:text-gray-200" for="emailAddress">Cambiar Correo</label>
                         <input id="emailAddress" type="email" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
 
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="password">Password</label>
+                        <label class="text-gray-700 dark:text-gray-200" for="password">Cambiar contraseña</label>
                         <input id="password" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
 
                     <div>
-                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Password Confirmation</label>
+                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Confirmar Contraseña</label>
                         <input id="passwordConfirmation" type="password" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                    </div>
+
+                    <div>
+                        <label class="text-gray-700 dark:text-gray-200" for="usu_profile">Subir imagen</label>
+                        <input  aria-describedby="user_avatar_help" id="usuprofile_help" type="file" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
                 </div>
 
                 <div class="flex justify-end mt-6">
-                    <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+                    <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Guardar</button>
                 </div>
             </form>
         </section>
@@ -46,4 +51,4 @@ const mapStateToProps = (state) => ({
 
 export default connect(mapStateToProps, {
 
-})(SeguirServ)
+})(PerfilUsu)
