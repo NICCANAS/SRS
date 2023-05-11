30de332248733028a8a1dc7dc396e5e20e04bc25
Jordanium01 <jordanurzua101@gmail.com>
2023-05-10 22:39:54 -0400

    validacion login-registro

--- src/components/log-in/login.js
+++ src/components/log-in/login.js
@@ -6,56 +6,72 @@ import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 function Login() {
-const [email, setEmail] = useState('');//Recoger el email del input
-const [password, setPassword] = useState('');//Recoger el password del input
+    const [email, setEmail] = useState('');//Recoger el email del input
+    const [password, setPassword] = useState('');//Recoger el password del input
 
-const formRef = useRef(null);//Cree una referencia al formulario, para poder utilizar submit en la funcion onSubmitHandler
+    const formRef = useRef(null);//Cree una referencia al formulario, para poder utilizar submit en la funcion onSubmitHandler
 
-//Establece el email a medida que el usuario va escribiendo
-const emailChange = event => {
-    setEmail(event.target.value);
-};
-//Establece la contraseña a medida que el usuario esta escribiendo
-const passwordChange = event => {
-    setPassword(event.target.value);
-};
+    //Establece el email a medida que el usuario va escribiendo
+    const emailChange = event => {
+        setEmail(event.target.value);
+    };
+    //Establece la contraseña a medida que el usuario esta escribiendo
+    const passwordChange = event => {
+        setPassword(event.target.value);
+    };
 
-//Consultar oracle sobre la contraseña del usuario
-async function oracleCheckUser(){
-    const params  = {
-      query: "SELECT COALESCE(COUNT(*), 0) FROM CLIENTE WHERE CORREO_CLI='"+email+"' AND CONTRA_CLI='"+password+"'",//las query no deben terminar en ";"
+    //Consultar oracle sobre la contraseña del usuario
+    async function oracleCheckUser() {
+        const params = {
+            query: "SELECT COALESCE(COUNT(*), 0) FROM CLIENTE WHERE CORREO_CLI='" + email + "' AND CONTRA_CLI='" + password + "'",//las query no deben terminar en ";"
+        }
+        console.log(params.query)
+        const res = await axios.get('http://127.0.0.1:8000/oracleAPI/', { params });
+
+        return res.data.query[0]//Devuelve un array con un dato, por lo que le pido el primero
     }
-    console.log(params.query)
-    const res = await axios.get('http://127.0.0.1:8000/oracleAPI/', {params});
 
-    return res.data.query[0]//Devuelve un array con un dato, por lo que le pido el primero
-} 
+    //Evento que maneja la validacion del formulario
+    async function onSubmitHandler(e) {
+        e.preventDefault();//Evitar que el formulario se envie
+        //--Validacion en front end--
+        //Validacion Email Vacio
+        if (email == "") {
+            alert('El campo email NO puede estar vacio!')
+            return;
+        }
 
-  //Evento que maneja la validacion del formulario
-async function onSubmitHandler(e){
-    e.preventDefault();//Evitar que el formulario se envie
-    console.log("Validando el submit");
-    let res = await oracleCheckUser();
-    console.log("resultado: "+res);
-    if (res == 1){
-        console.log("se subio el form")
-        //Despues de validar, enviar el formulario manualmente
-        formRef.current.submit();
-    } else {
-        console.log("poner aqui el validador")
+        if (password.length == 0) {
+            alert('El campo clave NO puede estar vacio!')
+            return;
+        }
+
+
+        //--Validacion en back end--
+        let res = await oracleCheckUser();
+        if (res == 1) {
+            console.log("se subio el form")
+            //Despues de validar, enviar el formulario manualmente
+            formRef.current.submit();
+        } else {
+            alert('El usuario o la contraseña son incorrectos')
+        }
+
     }
-    
-}
 
     return (
         <div class='h-screen bg-gradient-to-r from-pink-700 to-purple-900'>
             <section class="bg-gray-50 dark:bg-gray-900 h-screen bg-gradient-to-r from-green-400 to-purple-600">
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <SvgLogoUsado />
-                </a>
+                    <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <SvgLogoUsado />
+                    </a>
                     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <Link to="/" type="button" class=" rotate-180 flex flex-row text-indigo-700 border border-indigo-700 hover:bg-indigo-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-indigo-500 dark:text-indigo-500 dark:hover:text-white dark:focus:ring-indigo-800 dark:hover:bg-indigo-500">
+                                <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                                <span class="sr-only">Icon description</span>
+                            </Link>
                             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                                 Inicio de Sesion
                             </h1>
@@ -83,12 +99,12 @@ async function onSubmitHandler(e){
                                         </div>
                                     </div>
                                     {/* Boton olvidar contraseña */}
-                                    <a href="#" class="text-sm font-medium text-indigo-600 hover:underline dark:text-indigo-500">¿Olvidaste tu Contraseña?</a>
+                                    <Link to="/forgotpass" class="text-sm font-medium text-indigo-600 hover:underline dark:text-indigo-500">¿Olvidaste tu Contraseña?</Link>
                                 </div>
                                 {/* boton submit */}
                                 <button type="submit" class="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-primary-800">Ingresar</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    ¿No te encuentras registrado? 
+                                    ¿No te encuentras registrado?
                                     <Link to="/Register" className="font-medium text-indigo-600 hover:underline dark:text-indigo-500"> Registrarse</Link>
                                 </p>
                             </form>
--- src/components/register/register.js
+++ src/components/register/register.js
@@ -6,127 +6,169 @@ import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 function Register() {
-//Recojer combobox
-const [cancellUseEffect, setCancel] = useState(false);
-const [comunas, setComunas] = useState([]);//Establecer las comunas del combobox
-//Constantes del formulario
-const [rut, setRut] = useState(0);//Rut
-const [email, setEmail] = useState('');//Correo
-const [password, setPassword] = useState('');//contraseña
-const [Confpassword, setConfPassword] = useState('');//confirmar contraseña
-const [name, setName] = useState('');//nombre
-const [lastName, setLastName] = useState('');//apellido
-const [phone, setPhone] = useState('');//celular
-const [comunaId, setComunaId] = useState(0);//id comuna
+    //Recojer combobox
+    const [cancellUseEffect, setCancel] = useState(false);
+    const [comunas, setComunas] = useState([]);//Establecer las comunas del combobox
+    //Constantes del formulario
+    const [rut, setRut] = useState(null);//Rut
+    const [email, setEmail] = useState('');//Correo
+    const [password, setPassword] = useState('');//contraseña
+    const [Confpassword, setConfPassword] = useState('');//confirmar contraseña
+    const [name, setName] = useState('');//nombre
+    const [lastName, setLastName] = useState('');//apellido
+    const [phone, setPhone] = useState('');//celular
+    const [comunaId, setComunaId] = useState(0);//id comuna
 
-//Imagen del formulario
-const [image, setImage] = useState(null);//La imagen que se recojera del input (blob)
-const [imageUrl, setImageUrl] = useState('');//url de la imagen que devolvera github
+    //Imagen del formulario
+    const [image, setImage] = useState(null);//La imagen que se recojera del input (blob)
+    const [imageUrl, setImageUrl] = useState('');//url de la imagen que devolvera github
 
-//Establecer las constantes (set)
-const rutChange = event => { setRut(event.target.value); };
-const emailChange = event => { setEmail(event.target.value); };
-const passChange = event => { setPassword(event.target.value); };
-const confPassChange = event => { setConfPassword(event.target.value); };
-const nameChange = event => { setName(event.target.value); };
-const lastNameChange = event => { setLastName(event.target.value); };
-const phoneChange = event => { setPhone(event.target.value); };
+    //Establecer las constantes (set)
+    const rutChange = event => { setRut(event.target.value); };
+    const emailChange = event => { setEmail(event.target.value); };
+    const passChange = event => { setPassword(event.target.value); };
+    const confPassChange = event => { setConfPassword(event.target.value); };
+    const nameChange = event => { setName(event.target.value); };
+    const lastNameChange = event => { setLastName(event.target.value); };
+    const phoneChange = event => { setPhone(event.target.value); };
 
-//Crear una referencia del formukario
-const formRef = useRef(null);//Cree una referencia al formulario, para poder utilizar submit en la funcion onSubmitHandler
+    //Crear una referencia del formukario
+    const formRef = useRef(null);//Cree una referencia al formulario, para poder utilizar submit en la funcion onSubmitHandler
 
-//recojer id comuna del combobox
-const handleComunaChange = (event) =>{
-    let selectedOption = event.target.options[event.target.selectedIndex];
-    let value = selectedOption.value;
-    let label = selectedOption.text;
+    //recojer id comuna del combobox
+    const handleComunaChange = (event) => {
+        let selectedOption = event.target.options[event.target.selectedIndex];
+        let value = selectedOption.value;
+        let label = selectedOption.text;
 
-    console.log("1): "+value);//Esto es lo que se guardara en la base de datos del usuario
-    console.log("2): "+label);//Esto es el label de la opcion, lo puse solamente para ver si recogia bien el dato
-    //console.log("2): "+valor_sel);
-    setComunaId(value);//Establecer la id de la comuna
-}
-
-//Imagen del formulario (blob)
-const handleImageChange = (event) => {
-    setImage(event.target.files[0]);
-};
-
-///////////////////////////////Funciones
-useEffect(() => {
-    //Establecer las comunas del combobox
-    if (!cancellUseEffect)
-    {
-        setComunasOracle();
-        setCancel(true);
-    }
-    });
-
-//Recoger las comunas desde la base de datos
-async function setComunasOracle(){
-    let array = await returnOracle("SELECT ID_COM, NOM_COM FROM COMUNA");
-    setComunas(array);
-    console.log(array);
-}
-
-//API de oracle
-async function returnOracle(string){
-    const params  = {
-        query: string,//las query no deben terminar en ";"
-      }
-      const res = await axios.get('http://127.0.0.1:8000/oracleAPI/', {params});
-      return res.data.query;
-}
-
-//subir imagen a github y retornar url
-const uploadImage = async () => {
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(image);
-    fileReader.onload = async () => {
-      const base64Image = fileReader.result.split(',')[1];
-  
-    // Agrega tus propias credenciales
-    const GITHUB_USERNAME = 'Jordan108';
-    const GITHUB_TOKEN = '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';//Token de github, se elimina despues de 30 dias desde el 07/05
-    const REPO_NAME = 'reactImage';
-    const FILE_PATH = 'Clientes';
-    const FILE_NAME = 'CliProfile_'+rut+'.png';
-  
-    const uploadUrl = `https://api.github.com/repos/${GITHUB_USERNAME}/${REPO_NAME}/contents/${FILE_PATH}/${FILE_NAME}`;
-  
-    const response = await axios({
-      method: 'PUT',
-      url: uploadUrl,
-      headers: {
-        'Authorization': `token ${GITHUB_TOKEN}`,
-        'Content-Type': 'application/json'
-      },
-      data: {
-        message: `Upload ${FILE_NAME}`,
-        content: base64Image//formData.get('file')
-      }
-    });
-    setImageUrl(response.data.content.download_url)
-    //console.log(response.data.content.download_url);
-  };
-};
-
-//Funcion que valida el submit del formulario
-async function onSubmitHandler(e){
-    e.preventDefault();//Evitar que el formulario se envie
-    
-    //Primero tenemos que verificar si no existe un usuario que tenga ese rut o ese correo
-    let userExist = await returnOracle("SELECT COALESCE(COUNT(*), 0) FROM CLIENTE WHERE CORREO_CLI='"+email+"' AND RUT_CLI='"+rut+"'");
-    if (userExist > 0){
-        console.log("el usuario con el email: "+email+" y rut: "+rut+" existe, no sirve")
-        return //terminar la funcion submit sin subir
+        console.log("1): " + value);//Esto es lo que se guardara en la base de datos del usuario
+        console.log("2): " + label);//Esto es el label de la opcion, lo puse solamente para ver si recogia bien el dato
+        //console.log("2): "+valor_sel);
+        setComunaId(value);//Establecer la id de la comuna
     }
 
-    await uploadImage();//El propio script establece el url, con rut como id
-    //Insert del usuario en la base de datos
-    let sqlresponse = await returnOracle("INSERT INTO CLIENTE VALUES("+rut+",'"+name+"','"+lastName+"','"+email+"',"+comunaId+","+phone+",'"+password+"','"+imageUrl+"')");
-    formRef.current.submit();    
-}
+    //Imagen del formulario (blob)
+    const handleImageChange = (event) => {
+        setImage(event.target.files[0]);
+    };
+
+    ///////////////////////////////Funciones
+    useEffect(() => {
+        //Establecer las comunas del combobox
+        if (!cancellUseEffect) {
+            setComunasOracle();
+            setCancel(true);
+        }
+    });
+
+    //Recoger las comunas desde la base de datos
+    async function setComunasOracle() {
+        let array = await returnOracle("SELECT ID_COM, NOM_COM FROM COMUNA");
+        setComunas(array);
+        console.log(array);
+    }
+
+    //API de oracle
+    async function returnOracle(string) {
+        const params = {
+            query: string,//las query no deben terminar en ";"
+        }
+        const res = await axios.get('http://127.0.0.1:8000/oracleAPI/', { params });
+        return res.data.query;
+    }
+
+    //subir imagen a github y retornar url
+    const uploadImage = async () => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(image);
+        fileReader.onload = async () => {
+            const base64Image = fileReader.result.split(',')[1];
+
+            // Agrega tus propias credenciales
+            const GITHUB_USERNAME = 'Jordan108';
+            const GITHUB_TOKEN = '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';//Token de github, se elimina despues de 30 dias desde el 07/05
+            const REPO_NAME = 'reactImage';
+            const FILE_PATH = 'Clientes';
+            const FILE_NAME = 'CliProfile_' + rut + '.png';
+
+            const uploadUrl = `https://api.github.com/repos/${GITHUB_USERNAME}/${REPO_NAME}/contents/${FILE_PATH}/${FILE_NAME}`;
+
+            const response = await axios({
+                method: 'PUT',
+                url: uploadUrl,
+                headers: {
+                    'Authorization': `token ${GITHUB_TOKEN}`,
+                    'Content-Type': 'application/json'
+                },
+                data: {
+                    message: `Upload ${FILE_NAME}`,
+                    content: base64Image//formData.get('file')
+                }
+            });
+            setImageUrl(response.data.content.download_url)
+            //console.log(response.data.content.download_url);
+        };
+    };
+
+    function valFormFrontEnd(){
+        if(rut == "" || rut.length != 9 ){
+            alert('El Rut ingresado no es valido (Debe incluir el digito verificador, sin punto ni guion)')
+            return false;
+        }
+        
+        if(password == ""){
+            alert('Debe Ingresar contraseña')
+            return false;
+        }
+        
+        if(Confpassword == ""){
+            alert('Debe Ingresar Confirmar contraseña')
+            return false;
+        }
+        
+        if(password != Confpassword){
+            alert('Las contraseñas no Coinciden')
+            return false;
+        }
+        
+        if(name == ""){
+            alert('Debe Ingresar Nombre')
+            return false;
+        }
+        
+        if(lastName == ""){
+            alert('Debe Ingresar Apellido')
+            return false;
+        }
+        
+        if(phone == "" || phone.length < 8 || phone.length > 9){
+            alert('Ingrese Celular Valido')
+            return false;
+        }
+
+        return true
+    }
+
+    //Funcion que valida el submit del formulario
+    async function onSubmitHandler(e) {
+        e.preventDefault();//Evitar que el formulario se envie
+        //--Validacion en front end--
+        let varfront = valFormFrontEnd();
+        if (!varfront) return //Si la funcion devolvio negativo, terminar la funcion
+
+        //--Validacion en back end--
+        //Primero tenemos que verificar si no existe un usuario que tenga ese rut o ese correo
+        let userExist = await returnOracle("SELECT COALESCE(COUNT(*), 0) FROM CLIENTE WHERE CORREO_CLI='" + email + "' AND RUT_CLI='" + rut + "'");
+        if (userExist > 0) {
+            console.log("el usuario con el email: " + email + " y rut: " + rut + " existe, no sirve")
+            return //terminar la funcion submit sin subir
+        }
+
+        await uploadImage();//El propio script establece el url, con rut como id
+        //Insert del usuario en la base de datos
+        let sqlresponse = await returnOracle("INSERT INTO CLIENTE VALUES(" + rut + ",'" + name + "','" + lastName + "','" + email + "'," + comunaId + "," + phone + ",'" + password + "','" + imageUrl + "')");
+        formRef.current.submit();
+    }
 
     return (
         <section class="bg-gradient-to-r from-green-400 to-purple-600 h-auto w-auto">
@@ -136,6 +178,10 @@ async function onSubmitHandler(e){
                 </Link>
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <Link to="/Login" type="button" class=" rotate-180 flex flex-row text-indigo-700 border border-indigo-700 hover:bg-indigo-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center dark:border-indigo-500 dark:text-indigo-500 dark:hover:text-white dark:focus:ring-indigo-800 dark:hover:bg-indigo-500">
+                            <svg aria-hidden="true" class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                            <span class="sr-only">Icon description</span>
+                        </Link>
                         <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Registro Cuenta
                         </h1>
@@ -182,7 +228,7 @@ async function onSubmitHandler(e){
                                 <input onChange={confPassChange} value={Confpassword} type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="floating_repeat_password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirmar Contraseña</label>
                             </div>
-                            
+
                             <div class="grid md:grid-cols-2 md:gap-6">
                                 {/* Input nombre */}
                                 <div class="relative z-0 w-full mb-6 group">
@@ -206,7 +252,7 @@ async function onSubmitHandler(e){
                                     <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comuna</label>
                                     <select onChange={handleComunaChange} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         {comunas.map((cm) => (
-                                        <option value={cm[0]}>{cm[1]}</option>
+                                            <option value={cm[0]}>{cm[1]}</option>
                                         ))}
                                     </select>
                                 </div>
