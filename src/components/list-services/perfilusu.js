db5e99e92f20a73b53decfef768b2686093e3218
Nikolai Cañas <nic.canas@duocuc.cl>
2023-05-12 16:30:06 -0400

    Arreglar foto mamaguevo

--- src/components/list-services/perfilusu.js
+++ src/components/list-services/perfilusu.js
@@ -9,7 +9,7 @@ function PerfilUsu() {
                 <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                     <div>
                         <label class="text-gray-700 dark:text-gray-200" for="username">Nombre</label>
-                        <input id="username" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                        <input id="username" type="text" value='' class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
 
                     <div>
@@ -31,11 +31,19 @@ function PerfilUsu() {
                         <label class="text-gray-700 dark:text-gray-200" for="usu_profile">Subir imagen</label>
                         <input  aria-describedby="user_avatar_help" id="usuprofile_help" type="file" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
+
+                    <div class="ml-20">
+                        <label class="text-gray-700 dark:text-gray-200" for="passwordConfirmation">Foto Actual</label>
+                        <img class="h-12 rounded-full " alt='fotousu' src="https://avatars.githubusercontent.com/u/101841361?s=400&u=0c98e9da99e7f8e1d0d9c954d43d7c02470112bf&v=4" />
+                    </div>
+
                 </div>
 
                 <div class="flex justify-end mt-6">
                     <button class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Guardar</button>
                 </div>
+
+                
             </form>
         </section>
 
