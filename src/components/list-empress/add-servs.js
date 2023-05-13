7a30b08410bfcfc3765a6dad606bf3b8d7a105a0
NICCANAS <nic.canas@duocuc.cl>
2023-05-13 16:33:25 -0400

    Eliminación de fechas

--- src/components/list-empress/add-servs.js
+++ src/components/list-empress/add-servs.js
@@ -35,11 +35,6 @@ function AddServ() {
                         <input id="Dia_serv" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
 
-                    <div> {/* Hora del servicio */}
-                        <label class="text-gray-700 dark:text-gray-200" for="Hora_serv">Horas del servicio</label>
-                        <input id="Hora_serv" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
-                    </div>
-
                     <div> {/* Foto del servicio */}
                         <label class="text-gray-700 dark:text-gray-200" for="Foto_del_serv">Foto del servicio</label>
                         <input aria-describedby="Foto_del_serv" id="usuprofile_help" type="file" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
