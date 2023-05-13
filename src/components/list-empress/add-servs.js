45e720f5ead17ae447ac9d074afb02fffc01ce62
NICCANAS <nic.canas@duocuc.cl>
2023-05-13 16:36:23 -0400

    Update add-servs.js

--- src/components/list-empress/add-servs.js
+++ src/components/list-empress/add-servs.js
@@ -20,7 +20,7 @@ function AddServ() {
                         <input id="descrp_serv" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
 
-                    <div> {/* Por la xita joldan, Valor servicio */}
+                    <div> {/* Valor servicio */}
                         <label class="text-gray-700 dark:text-gray-200" for="valor_serv">Valor servicio</label>
                         <input id="valor_serv" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
                     </div>
