c2338152788b6dc96188bef4ac653fbc4c596026
NICCANAS <nic.canas@duocuc.cl>
2023-05-13 14:01:01 -0400

    Cambios text

--- src/components/list-services/listservs.js
+++ src/components/list-services/listservs.js
@@ -12,20 +12,20 @@ function ListServs() {
     const [active, setActive] = useState("Cardserv")
 
     return (
-        <div class="flex flex-wrap bg-gray-100 w-full h-screen">
-            <div class="w-3/12 bg-white rounded p-3 shadow-lg">
-                <div class="flex items-center space-x-4 p-2 mb-5">
-                    <img class="h-12 rounded-full" alt='fotousu' src="https://avatars.githubusercontent.com/u/101841361?s=400&u=0c98e9da99e7f8e1d0d9c954d43d7c02470112bf&v=4" />
+        <div className="flex flex-wrap bg-gray-100 w-full h-screen">
+            <div className='bg-white w-3/12 rounded p-3'  >
+                <div className="flex items-center space-x-4 p-2 mb-5">
+                    <img className="h-12 rounded-full" alt='fotousu' src="https://avatars.githubusercontent.com/u/101841361?s=400&u=0c98e9da99e7f8e1d0d9c954d43d7c02470112bf&v=4" />
                     <div>
-                        <h4 class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">Nikolai</h4>
-                        <span class="text-sm tracking-wide flex items-center space-x-1">
+                        <h4 className="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">Nikolai</h4>
+                        <span className="text-sm tracking-wide flex items-center space-x-1">
                         </span>
                     </div>
                 </div>
-                <ul class="space-y-2 text-sm">
+                <ul className="space-y-2 text-sm">
                     <li>
-                        <a onClick={() => setActive("Cardserv")} class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 bg-gray-200 focus:shadow-outline">
-                            <span class="text-gray-600">
+                        <a onClick={() => setActive("Cardserv")} className="flex items-center space-x-3  p-2 rounded-md font-medium hover:bg-gray-200  focus:shadow-outline">
+                            <span className="text-gray-600">
                                 <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
                                 </svg>
@@ -35,8 +35,8 @@ function ListServs() {
                     </li>
                     <li>
                         <a onClick={() => setActive("SeguirServ")} class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 focus:bg-gray-200 focus:shadow-outline">
-                            <span class="text-gray-600">
-                                <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <span className="text-gray-600">
+                                <svg className="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                                 </svg>
                             </span>
@@ -46,8 +46,8 @@ function ListServs() {
 
                     <li>
                         <a onClick={() => setActive("HistoUsu")} class="flex items-center space-x-3 text-gray-700 p-2 rounded-md font-medium hover:bg-gray-200 focus:bg-gray-200 focus:shadow-outline">
-                            <span class="text-gray-600">
-                                <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <span className="text-gray-600">
+                                <svg className="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                                 </svg>
                             </span>
