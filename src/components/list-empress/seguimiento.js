19f278f10e4dcbd0747b304939b31f1ee03b4b5a
Jordan101 <jordanurzua101@gmail.com>
2023-05-15 17:08:38 -0400

    final final ahora si

--- src/components/list-empress/seguimiento.js
+++ src/components/list-empress/seguimiento.js
@@ -8,12 +8,27 @@ function SeguiServ() {
 
     const [cancellUseEffect, setCancel] = useState(false);
     const [servicios, setServicios] = useState([]);//Establecer los servicios
+    const [estOrdenes, setEstOrdenes] = useState([]);
+
+    const handleEstadoChange = (arg) => (event) => {
+        let selectedOption = event.target.options[event.target.selectedIndex];
+        let value = selectedOption.value;
+        let label = selectedOption.text;
+
+        console.log("1): " + value);//Esto es lo que se guardara en la base de datos del usuario
+        console.log("2): " + label);//Esto es el label de la opcion, lo puse solamente para ver si recogia bien el dato
+        
+        returnOracle("UPDATE ORDEN_SERV SET ESTADO_SERV_ID_ESTADO="+value+" WHERE ID_ORD='" + arg + "'");
+        //console.log("2): "+valor_sel);
+        //setComunaId(value);//Establecer la id de la comuna
+    }
 
     //////////FUNCIONES
     useEffect(() => {
         //Establecer las ordenes creadas por el usuario
         if (!cancellUseEffect) {
             setServiciosOracle();
+            setEstadoOrdenesOracle();
             setCancel(true);
         }
     });
@@ -30,8 +45,16 @@ function SeguiServ() {
     //Recoger las ordenes de servicios de la bdd
     async function setServiciosOracle() {
         //React no me deja hacer la sentencia en mas de una linea
-        let array = await returnOracle("SELECT CLI.NOM_CLI, CLI.APE_CLI, SERV.NOM_SERV, ORD.FECHA_INI, EST.NOM_ESTADO FROM ORDEN_SERV ORD INNER JOIN SERVICIO SERV ON ORD.SERVICIO_ID_SERV=SERV.ID_SERV INNER JOIN ESTADO_SERV EST ON ORD.ESTADO_SERV_ID_ESTADO=EST.ID_ESTADO INNER JOIN CLIENTE CLI ON ORD.CLIENTE_RUT_CLI=CLI.RUT_CLI WHERE SERV.EMPRESA_RUT_EMP='" + userID + "'");
+        let array = await returnOracle("SELECT CLI.NOM_CLI, CLI.APE_CLI, SERV.NOM_SERV, ORD.FECHA_INI, EST.ID_ESTADO, EST.NOM_ESTADO, ORD.ID_ORD FROM ORDEN_SERV ORD INNER JOIN SERVICIO SERV ON ORD.SERVICIO_ID_SERV=SERV.ID_SERV INNER JOIN ESTADO_SERV EST ON ORD.ESTADO_SERV_ID_ESTADO=EST.ID_ESTADO INNER JOIN CLIENTE CLI ON ORD.CLIENTE_RUT_CLI=CLI.RUT_CLI WHERE SERV.EMPRESA_RUT_EMP='" + userID + "' AND ORD.ORD_ESTADO_ID_PAGO=1");
         setServicios(array);
+        console.log(array);
+    }
+
+    //Recoger el estado de las ordenes
+    async function setEstadoOrdenesOracle() {
+        //React no me deja hacer la sentencia en mas de una linea
+        let array = await returnOracle("SELECT ID_ESTADO, NOM_ESTADO FROM ESTADO_SERV");
+        setEstOrdenes(array);
         console.log(array);
     }
 
@@ -58,9 +81,13 @@ function SeguiServ() {
                                             class="px-5 py-3 border-b-2  bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                             Fecha
                                         </th>
-                                        <th
+                                        {/* <th
                                             class="px-5 py-3 border-b-2  bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
                                             Estado
+                                        </th> */}
+                                        <th
+                                            class="px-5 py-3 border-b-2  bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                                            Modificar Estado
                                         </th>
                                     </tr>
                                 </thead>
@@ -90,13 +117,27 @@ function SeguiServ() {
                                                 </p>
                                             </td>
                                             {/* Aca siempre serian los activos */}
-                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                           {/*  <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <span
                                                     class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                                                     <span aria-hidden
                                                         class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                    <span class="relative">{serv[4]}</span>
+                                                    <span class="relative">{serv[5]}</span>
                                                 </span>
+                                            </td> */}
+                                            {/* Modificar estado */}
+                                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                               {/*  <span
+                                                    class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                    <span aria-hidden
+                                                        class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                    <span class="relative">{serv[4]}</span> */}
+                                                    <select onChange={handleEstadoChange(serv[6])} value={serv[4]} id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                        {estOrdenes.map((est) => (
+                                                            <option value={est[0]}>{est[1]}</option>
+                                                        ))}
+                                                    </select>
+                                                {/* </span> */}
                                             </td>
                                         </tr>
                                     ))}
