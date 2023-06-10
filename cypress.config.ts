6d9875f4ff8fc27ca2dab9af04bccdd5515fe2ce
Jordanium01 <jordanurzua101@gmail.com>
2023-06-09 23:59:57 -0400

    crypress config + pf1-2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
     },
   },
 });
--- cypress/e2e/PruebaF02.cy.js
+++ cypress/e2e/PruebaF02.cy.js
@@ -8,24 +8,28 @@ describe('e2e-pruebaFuncional01', () => {
     cy.get('button').click()
     //Presionar el boton de registrarse
     cy.contains('Registrarse').click()
+    //Boton del formulario de empresas
+    cy.contains('Empresa / Pymes').click()
 
     //Dentro de registrarse, rellenar los datos del formulario
     //Rut
-    cy.get('input[name="floating_rut"]').type('216783219')
+    cy.get('input[name="floating_rut"]').type('197672108')
+    //Tipo empresa
+    cy.get('[id=Empresa]').select('Empresa')
     //Correo
-    cy.get('input[name="floating_email"]').type('nxt322@gmail.com')
+    cy.get('input[name="floating_email"]').type('pruebaempresa1@gmail.com')
     //Contraseña
-    cy.get('input[name="floating_password"]').type('jose1')
+    cy.get('input[name="floating_password"]').type('empresa1')
     //repetir contraseña
-    cy.get('input[name="repeat_password"]').type('jose1')
+    cy.get('input[name="repeat_password"]').type('empresa1')
     //Nombre
-    cy.get('input[name="floating_first_name"]').type('Jose')
-    //Apellido
-    cy.get('input[name="floating_last_name"]').type('Cabrera')
+    cy.get('input[name="floating_email"]').type('Empresa Prueba')
+    //Direccion
+    cy.get('input[name="floating_last_name"]').type('Condell 345')
     //CElular
     cy.get('input[name="floating_phone"]').type('986324342')
     //Combobox
-    cy.get('[id="countries"]').select('Iquique')
+    cy.get('[id="countries"]').select('Maipú')
     //Insertar imagen
     const fileImage = 'cypress/e2e/testImages/testUserImage.png';
     cy.get('[id="user_avatar"]').selectFile(fileImage)
